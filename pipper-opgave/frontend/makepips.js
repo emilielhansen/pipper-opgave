fc74de266e93d331d13b312d7c43afe436fdfa75
emilielhansen <56879069+emilielhansen@users.noreply.github.com>
2022-09-14 15:59:41 +0200

    Makepips js til at lave post delen

---
+++ pipper-opgave/frontend/makepips.js
@@ -0,0 +1,9 @@
+function whenUserSubmitPip() {
+    const user = document.getElementById("username").value
+    const pip = document.getElementById("pip").value
+
+    fetch("http://localhost:8000", {
+        method: "POST",
+
+    })
+}
