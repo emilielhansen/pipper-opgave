b66f382e50679cb4c80cd7c2cb8d5027fb86ae23
emilielhansen <56879069+emilielhansen@users.noreply.github.com>
2022-09-14 16:02:31 +0200

    Opdateret læs pip

--- pipper-opgave/frontend/readpips.js
+++ pipper-opgave/frontend/readpips.js
@@ -1,34 +1,58 @@
-/*
-async function callApi() {​
-    let result = await fetch('http://localhost:8000');​
-    result = await result.json();​
-    console.log(result);​
-}​
+// LÆS PIP FRA DB 
 
-callApi();
-*/
-
-function whenUserSubmitPip() {
-    const user = document.getElementById("username").value
-    const pip = document.getElementById("pip").value
-
-    fetch("http://localhost:8000", {
-        method: "POST",
-
-    })
-
-}
-
-async function test() {
+// Først skal vi hente vores json-fil fra DB
+async function readPip() {
     const result = await fetch("http://localhost:8000");
-    console.log(result);
-    const body = await result.json();
-    console.log(body)
-    // [ {}, {}, {} ]
-    body.forEach(function (pip) {
-        console.log(pip);
+    const pipFromDB = await result.json();
+
+    // Så henter vi hvert element fra vores objekt
+    const items = [];
+        for(pip in pipFromDB){
+        items.push(pipFromDB[pip]);
+        }
+
+    // Vi looper hen over hvert array i DB
+    pipFromDB.forEach(function (pip) {
+
+        // Opret en div hvori du indsætter dine pip-værdier
+        // Vores objekt ser sådan ud:
+
+        /* 
+            {
+               "pipID": "1",
+               "username": "denitsa1234",
+               "pipmessage": "Hello World!",
+               "datetime": "2022-09-09 10:15:36"
+            },
+        */    
+
+        // 
+        const element = 
+        `
+        <div class ="pip-boks">
+        <div class="bruger">
+            <div class="avatar"><img src="https://xsgames.co/randomusers/avatar.php?g=pixel"
+                    alt="billede" id="billede_avatar"></div>
+
+            <div class="brugernavn">
+                <h3 id="person">@${pip.username}</h3>
+            </div>
+        </div>
+        
+        <div class="pipBesked">
+            <p>${pip.pipmessage}</p>
+        </div>
+        </div>`
+
+    //Så skal vi tilføje elementet i vores HTML, det gør vi med querySelector.
+    //Inde i gåseøjenene skriver vi navnet på den container vi har lavet i vores html 
+
+    const div = document.querySelector(".container");
+    div.innerHTML += element;
     });
 }
 
-test();
+readPip();
+
+
 
