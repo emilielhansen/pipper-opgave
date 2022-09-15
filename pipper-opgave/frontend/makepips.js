1505faa900b7552e86629d13cf0f01df5906d5b9
deni03030 <102724084+deni03030@users.noreply.github.com>
2022-09-15 10:56:35 +0200

    Update makepips.js

--- pipper-opgave/frontend/makepips.js
+++ pipper-opgave/frontend/makepips.js
@@ -2,8 +2,13 @@ function whenUserSubmitPip() {
     const user = document.getElementById("username").value
     const pip = document.getElementById("pip").value
 
+    const asObject = {
+        username: user,
+        pipbesked: pip,
+    }
+
     fetch("http://localhost:8000", {
         method: "POST",
-
-    })
+    body: JSON.stringify(asObject)
+})
 }
