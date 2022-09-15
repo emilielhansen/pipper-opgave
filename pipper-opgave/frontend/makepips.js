6e3b9b759d2e6412fca69aff052c5ee6ff44b613
deni03030 <102724084+deni03030@users.noreply.github.com>
2022-09-15 13:07:55 +0200

    Update makepips.js

--- pipper-opgave/frontend/makepips.js
+++ pipper-opgave/frontend/makepips.js
@@ -18,17 +18,28 @@ function whenUserSubmitPip() {
 
 
 //START
-function whenUserSubmitPip() {
-    const user = document.getElementById("username").value
-    const pip = document.getElementById("pip").value
 
+// Get the Form element from our the DOM.
+const form = document.querySelector("#note-form");
+
+// Add an event listener on the form, listening for the "submit" event
+form.addEventListener("submit", (event) => {
+  // We prevent the default behaviour of the form, so it doesnt reload the page.
+  event.preventDefault();
+
+  // Get the user input value from the title input
+  // Get the user input value from the note textarea
+
+    const user = document.getElementById("indtast_brugernavn").value
+    const pip = document.getElementById("indtast_pip").value
+console.log(user, pip)
     const asObject = {
         username: user,
-        pipbesked: pip,
+        pipmessage: pip,
     }
 
     fetch("http://localhost:8000", {
         method: "POST",
     body: JSON.stringify(asObject)
 })
-}
+});
