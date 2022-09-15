75690cf96f4e80e75356dfac92c846f8a3690662
deni03030 <102724084+deni03030@users.noreply.github.com>
2022-09-15 12:54:03 +0200

    Update makepips.js

--- pipper-opgave/frontend/makepips.js
+++ pipper-opgave/frontend/makepips.js
@@ -12,3 +12,23 @@ function whenUserSubmitPip() {
     body: JSON.stringify(asObject)
 })
 }
+
+
+
+
+
+//START
+function whenUserSubmitPip() {
+    const user = document.getElementById("username").value
+    const pip = document.getElementById("pip").value
+
+    const asObject = {
+        username: user,
+        pipbesked: pip,
+    }
+
+    fetch("http://localhost:8000", {
+        method: "POST",
+    body: JSON.stringify(asObject)
+})
+}
