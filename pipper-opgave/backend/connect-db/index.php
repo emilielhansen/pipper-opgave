7781ac84f58da8923a8030a3211b7779d3b1530d
deni03030 <102724084+deni03030@users.noreply.github.com>
2022-09-15 12:52:33 +0200

    Update index.php

--- pipper-opgave/backend/connect-db/index.php
+++ pipper-opgave/backend/connect-db/index.php
@@ -51,3 +51,69 @@ catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
 ?>
+
+
+
+
+
+
+
+
+//START Denitsa
+<?php
+
+// CONNECT TIL DB
+// Inkluder til password i din .env fil
+
+
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+// Her connecter du til databasen
+
+// Hvor din database skal køre
+$servername = "localhost:3306";
+
+// Dit brugernavn i mySQL
+$username = "root";
+
+// Dit password som du henter fra .env filen
+$password = 'sivataMechka2022';
+
+if ($requestType == "GET") {
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=test", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $statement = $conn->query("select * from pips");
+    $result = $statement->fetchAll();
+
+    echo json_encode($result);
+  } catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+} elseif ($requestType == "POST") {
+  try {
+    $connection = getDbConnection();
+    $statement = $connection->prepare($statement);
+    $statement->execute(array(
+      'username' => $input['username'],
+      'pip' => $input['pip'],
+    ));
+
+    $id = $connection->lastInsertId();
+    $pip = (object) $input;
+    $pip->id = $id;
+
+    $response['status_code_header'] = 'HTTP/1.1 201 Created';
+    $response['body'] = json_encode($pip);
+    return $response;
+  } catch (\PDOException $e) {
+    exit($e->getMessage());
+  }
+}
